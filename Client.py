c8f248858115bfde650bfaee9897cf6bc2803835
Vincenzo Donadio <71927688+DonnyD-01@users.noreply.github.com>
2022-11-08 15:43:48 +0100

    Creazione file JSON con informazioni del sistema
    Il client ora crea automaticamente un file JSON in cui inserisce le informazioni inviate dal server

--- Client.py
+++ Client.py
@@ -1,4 +1,4 @@
-import json,socket
+import json,socket,os
 
 #Setting variables for connection
 serverName = 'localhost'
@@ -8,8 +8,16 @@ serverPort = 12000
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
+#If file exists, delete it
+filepath = "SystemInformation.JSON"
+if os.path.isfile(filepath):
+	os.remove(filepath)
+jsonfile = open(filepath, 'a')
+
 #Reception and decoding of the messages sent by the server
 for x in range (8):
 	modifiedSentence = clientSocket.recv(1024)
+	jsonfile.write(modifiedSentence.decode())
 	print(modifiedSentence.decode())
+jsonfile.close()
 clientSocket.close()
