794996d698e7709476981b3b57cb87d8d4dce6cc
asSparky <giuse.maio13@gmail.com>
2022-11-19 20:55:58 +0100

    Revert "Implementazione del salvataggio dei dati in subdirectory con naming basato su OS e timestamp"
    This reverts commit 0eb807ff641369e292a47c9db22a545693f81e88.

--- Client.py
+++ Client.py
@@ -1,5 +1,4 @@
 import json,socket,os
-from datetime import datetime
 
 #Setting variables for connection
 serverName = 'localhost'
@@ -10,26 +9,14 @@ bufferSize = 65536
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
+#Creating the file with the Information 
+jsonfile = open("SystemInformation.JSON", 'w')
+
 #Reception and decoding of the messages sent by the server
-info = clientSocket.recv(bufferSize).decode()
+info = clientSocket.recv(bufferSize)
+jsonfile.write(info.decode())
+print(info.decode())
+
+#Closing file and connection
+jsonfile.close()
 clientSocket.close()
-infoJson = json.loads(info)
-
-#Building the path of the direcotry to create based on OS and timestamp
-if(infoJson['platformInfo']['platform'] == "Windows"):
-    path = "Retrieved/" + infoJson['platformInfo']['platform'] + " " + infoJson['platformInfo']['platform-version'] + " " + datetime.now().strftime("%d-%m-%Y %H-%M-%S")
-else:
-    path = "Retrieved/" + infoJson['platformInfo']['platform'] + " " + infoJson['platformInfo']['platform-release'] + " " + datetime.now().strftime("%d-%m-%Y %H-%M-%S")
-
-#Creation of server's subdirectory if doesn't exist
-if not os.path.isdir(path):
-    try:
-        os.makedirs(path)
-        jsonFile = open(path + "/SystemInformation.JSON", 'w')
-        jsonFile.write(info)
-        jsonFile.close()
-    except OSError:
-        print (f"Creation of the subdirectory \"{path}\" failed")
-
-#Print of all the system information of the server
-print(info)
