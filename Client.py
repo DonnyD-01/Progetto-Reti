f6abe4f2c3020270d65442858ea539e5038c1b53
asSparky <giuse.maio13@gmail.com>
2022-11-19 20:58:02 +0100

    Implementazione del salvataggio dei dati in subdirectory con naming basato su OS e timestamp

--- Client.py
+++ Client.py
@@ -1,4 +1,5 @@
 import json,socket,os
+from datetime import datetime
 
 #Setting variables for connection
 serverName = 'localhost'
@@ -9,14 +10,26 @@ bufferSize = 65536
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-#Creating the file with the Information 
-jsonfile = open("SystemInformation.JSON", 'w')
-
 #Reception and decoding of the messages sent by the server
-info = clientSocket.recv(bufferSize)
-jsonfile.write(info.decode())
-print(info.decode())
-
-#Closing file and connection
-jsonfile.close()
+info = clientSocket.recv(bufferSize).decode()
 clientSocket.close()
+infoJson = json.loads(info)
+
+#Building the path of the direcotry to create based on OS and timestamp
+if(infoJson['platformInfo']['platform'] == "Windows"):
+    path = "Retrieved/" + infoJson['platformInfo']['platform'] + " " + infoJson['platformInfo']['platform-version'] + " " + datetime.now().strftime("%d-%m-%Y %H-%M-%S")
+else:
+    path = "Retrieved/" + infoJson['platformInfo']['platform'] + " " + infoJson['platformInfo']['platform-release'] + " " + datetime.now().strftime("%d-%m-%Y %H-%M-%S")
+
+#Creation of server's subdirectory if doesn't exist
+if not os.path.isdir(path):
+    try:
+        os.makedirs(path)
+        jsonFile = open(path + "/SystemInformation.JSON", 'w')
+        jsonFile.write(info)
+        jsonFile.close()
+    except OSError:
+        print (f"Creation of the subdirectory \"{path}\" failed")
+
+#Print of all the system information of the server
+print(info)
